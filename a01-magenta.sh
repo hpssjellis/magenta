d8f0248472597672ccfb4d084847e102f527eecb
Jeremy Ellis <keyfreemusic@gmail.com>
2019-06-18 22:00:35 -0700

    Update a01-magenta.sh

--- a01-magenta.sh
+++ a01-magenta.sh
@@ -3,9 +3,9 @@
 pip install magenta
 
               
-git clone https://github.com/tensorflow/magenta-demos.git  /home/gitpod/workspace/magenta/demos                  
-git clone https://github.com/googlecreativelab/aiexperiments-ai-duet.git   /home/gitpod/workspace/magenta/demos/ai-duet         
-git clone https://github.com/chrisdonahue/piano-genie-research-demo.git    /home/gitpod/workspace/magenta/demos/piano-genie-js  
+git clone https://github.com/tensorflow/magenta-demos.git                   
+git clone https://github.com/googlecreativelab/aiexperiments-ai-duet.git            
+git clone https://github.com/chrisdonahue/piano-genie-research-demo.git   
            
            
            
